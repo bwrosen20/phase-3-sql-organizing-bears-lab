76969217be6246436e6fd6e5fc662ace8de56311
bwrosen20 <bwrosen20@gmail.com>
2023-06-07 10:03:45 -0400

    commit tree 8e882db5107cdf1a4886d7423c74fc4ea4a373c9

---
+++ lib/insert.sql
@@ -0,0 +1,19 @@
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES 
+-- (1,"Chubby", 14, "M", "black", "sleepy", 1),
+-- (2,"Maxine", 2, "F", "brown", "active", 1),
+-- (3,"Lawrence", 14, "M", "black", "energetic", 0),
+-- (4,"Felula", 10, "F", "white", "kind", 1),
+-- (5,"Tim", 13, "M", "brown", "dopey", 0,
+-- (6,"Gail", 9, "F", "brown", "tired", 1),
+-- (7,"Happy", 7, "M", "black", "playful", 1),
+-- (8,null, 1, "F", "white", "cute", 0);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
